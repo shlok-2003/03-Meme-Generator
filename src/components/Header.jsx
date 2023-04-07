2004431fc070c245d45604e0b254db82b7b7ead9
Shlok Prajapati <shlokprajapati2003@gmail.com>
2023-04-07 17:59:07 +0530

    Initial changes

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './Header.css'
+import logo from '../logo.png'
+
+function Header() {
+    return (
+        <header className='header--main'>
+            <img src={logo} alt="" className='header--img'/>
+            <span className='header--mainText'>Meme Generator</span>
+
+            <h3 className='header--authorName'>By Shlok Prajapati</h3>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Meme.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react'
+import './Meme.css'
+
+function Meme() {
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        url: ""
+    })
+
+    const [allMeme, setAllMeme] = useState([])
+
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(file => setAllMeme(file.data.memes))
+    }, [])
+
+    function getMemeImage() {
+        const randomIndex = Math.floor(Math.random() * (allMeme.length - 0) + 0)
+
+        setMeme(prevData => {
+            return {
+                ...prevData,
+                url: allMeme[randomIndex].url,
+            }
+        })  
+
+        document.getElementById('meme--main').classList.add('meme--main-active')
+    }
+
+    function getText(event) {
+        const {name, value} = event.target
+
+        setMeme(prevData => {
+            return {
+                ...prevData,
+                [name]: value
+            }
+        })
+    }
+    
+    return (
+        <main>
+            <div className='meme--form'>
+                <div className='meme--form-input'>
+                    <input 
+                        type='text' 
+                        placeholder='Top Text'
+                        name="topText"
+                        onInput={getText}
+                        value={meme.topText}
+                        />
+                    <input 
+                        type='text' 
+                        placeholder='Bottom Text'
+                        name="bottomText"
+                        onInput={getText}
+                        value={meme.bottomText}
+                    />
+                </div>
+
+                <button className='meme--form-submit-btn' onClick={getMemeImage}>Get a new meme image 🖼</button>
+            </div>
+
+            <div className='meme--main' id='meme--main'>
+                <img src={meme.url} key={1} className='meme--main-img'/>
+                <h1 className='meme--main-top'>{meme.topText}</h1>
+                <h1 className='meme--main-bottom'>{meme.bottomText}</h1>
+            </div>
+        </main>
+    )
+}
+
+export default Meme
